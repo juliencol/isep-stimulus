8cc4473e26349799a7c475fbab6453fa3b62f193
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2020-04-17 00:42:18 +0200

    debut

---
+++ app/views/userResults.php
@@ -0,0 +1,26 @@
+<?php
+?>
+<html>
+    <link rel="stylesheet" href="styleCSSUserTest.css">
+    <title>Results</title>
+    <div id="container">
+        <header>
+            <a class="headerFooter">FAQ</a> <a class="headerFooter">Accéder au profil</a>
+        </header>
+        <section>
+            <aside>
+                <div id="photo"></div>
+                <button>Changer la photo</button>
+            </aside>
+            <article>
+                <h2>UTILISATEURS</h2>
+                <p>Nom : Prénom : </p>
+                <p>Date de naissance : </p>
+                <p>Numéro d'employé : </p>
+                <p>Entreprise : </p>
+                <button>Editer le profil</button>
+            </article>
+        </section>
+    </div>
+
+</html>
--- app/views/userTestResults.php
+++ app/views/userTestResults.php
@@ -1,2 +1,18 @@
 <?php
 ?>
+<html>
+    <link rel="stylesheet" href="styleCSSUserTest.css">
+    <title>Results</title>
+    <div id="container">
+    <header>
+        <a class="headerFooter">FAQ</a> <a class="headerFooter">Accéder au profil</a>
+    </header>
+    <h1>Temps de réaction à un son</h1>
+    <h1>Capacité à reproduire un son</h1>
+    <h1>Temps de réaction à une lumière</h1>
+    <footer>
+        <a class="headerFooter">Nous contacter</a> <a class="headerFooter">CGU et metions légales</a><small class="headerFooter">Copyright Stimulus Inc.</small>
+    </footer>
+</div>
+
+</html>
