c94b1cc28759f298eb7579874b51420bcaa6601c
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2020-04-17 00:47:49 +0200

    debut

--- app/views/userResults.php
+++ app/views/userResults.php
@@ -2,7 +2,7 @@
 ?>
 <html>
     <link rel="stylesheet" href="styleCSSUserTest.css">
-    <title>Results</title>
+    <title>USER Results</title>
     <div id="container">
         <header>
             <a class="headerFooter">FAQ</a> <a class="headerFooter">Accéder au profil</a>
@@ -21,6 +21,9 @@
                 <button>Editer le profil</button>
             </article>
         </section>
+        <h1>Consulter les résultats de ses tests</h1>
+        <footer><a class="headerFooter">Nous contacter</a> <a class="headerFooter">CGU et metions légales</a><small class="headerFooter">Copyright Stimulus Inc.</small>
+        </footer>
     </div>
 
 </html>
