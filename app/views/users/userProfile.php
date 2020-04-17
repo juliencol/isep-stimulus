25124ee2240fd9a501a7e8f9d06f974cc119c65f
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2020-04-17 18:03:54 +0200

    changements

--- app/views/users/userProfile.php
+++ app/views/users/userProfile.php
@@ -1,11 +1,10 @@
 <?php
 ?>
 <html>
-    <link rel="stylesheet" href="styleCSSUserTest.css">
-    <title>USER Results</title>
+    <link rel="stylesheet" href="../../stylesheets/styleCSSUserTest.css">
+    <title>USER Profile</title>
     <div id="container">
         <header>
-            <a class="headerFooter">FAQ</a> <a class="headerFooter">Accéder au profil</a>
         </header>
         <section>
             <aside>
@@ -13,7 +12,7 @@
                 <button>Changer la photo</button>
             </aside>
             <article>
-                <h2>UTILISATEURS</h2>
+                <h2>UTILISATEUR</h2>
                 <p>Nom : Prénom : </p>
                 <p>Date de naissance : </p>
                 <p>Numéro d'employé : </p>
@@ -21,9 +20,8 @@
                 <button>Editer le profil</button>
             </article>
         </section>
-        <h1>Consulter les résultats de ses tests</h1>
-        <footer><a class="headerFooter">Nous contacter</a> <a class="headerFooter">CGU et metions légales</a><small class="headerFooter">Copyright Stimulus Inc.</small>
-        </footer>
+        <h1 onclick="">Consulter les résultats de ses tests</h1>
+        <footer></footer>
     </div>
 
 </html>
