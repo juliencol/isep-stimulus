e86b51348fd7cb7b0e0d872b71cf00aaaf3cf50b
Celia <celiahoulette@yahoo.fr>
2020-04-18 16:06:13 +0200

    création des pages de tests

---
+++ pages tests/light_test_1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Sound test</title>
+	<link rel="stylesheet" type="text/css" href="sound_test_1.css">
+</head>
+<body>
+
+	<header>
+		
+	</header>
+
+
+	<h1>Temps de réaction à une lumière </h1>
+	
+	<p class="text_explicatif">
+		Ce test dure approximativement 2 minutes. <br> <br>
+		Lorsque vous voyez une des trois LEDs devant vous s'alllumer, appuyez sur le bouton qui lui correspond.  <br> <br>
+		Il s'agit d'un test de rapidité. Plus vous êtes rapide à appuyer sur le bon bouton, plus votre score sera élevé. <br> <br>
+		Cependant, prenez garde à ne pas appuyer avant qu'une des trois LEDs ne s'allume et à ne pas vous tromper de bouton. Dans ce cas, votre score sera impacté ou vous devrez recommencer le test. 
+
+	</p>
+		
+	<div id="bouton">
+
+		<a href="sound_test_code.php">Commencer le test</a>	
+
+	</div>
+
+	<footer>
+		
+	</footer>
+
+</body>
+
+</html>
---
+++ pages tests/test_confirmation.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Stimulus</title>
+	<link rel="stylesheet" type="text/css" href="test_confirmation.css">
+
+</head>
+<body>
+
+<header>
+	
+</header>
+
+<h1>Les résultats du test ont été enregistrés avec succès </h1>
+
+<div id="bouton">
+	
+	<p><a href="#">Voir les résultats</a></p>
+
+	<br><br><br>
+
+	<p><a href="#">Retour au profil </a></p>
+
+</div>
+
+<footer>
+	
+</footer>
+
+</body>
+</html>
