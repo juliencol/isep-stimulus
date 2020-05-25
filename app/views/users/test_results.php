422be9ae2ce9aee636e301cae644d2fcad514680
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2020-05-26 00:16:16 +0200

    Header of the test results of user page

--- app/views/users/test_results.php
+++ app/views/users/test_results.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/../public/css/style.php'; ?>
 <style> <?php include APPROOT . '/../public/css/style.css'; ?> </style>
 <style> <?php include APPROOT . '/../public/css/users/test_results.css'; ?> </style>
-<?php //require APPROOT . '/views/inc/header.php'; ?>
+<?php require APPROOT . '/views/inc/header.php'; ?>
 
 <html>
     <title>Results</title>
