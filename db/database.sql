73006297f9d64e6e1f2ef65445687b6ff26f1253
cyprian <cyprinanou@gmail.com>
2020-05-28 14:54:17 +0200

    new database

--- db/database.sql
+++ db/database.sql
@@ -1,84 +1,170 @@
-CREATE TABLE `users` (
-  `id` integer,
-  `supervisor_id` integer,
-  `first_name` varchar(255),
-  `last_name` varchar(255),
-  `email` varchar(255),
-  `password` varchar(255),
-  `birthday_date` date,
-  `company` varchar(255),
-  `employee_number` integer,
-  `is_female` boolean,
-  `profile_picture` varchar(255),
-  `number_of_tests_done` integer,
-  `needs_tests` boolean
-);
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 28 mai 2020 à 12:47
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.3.12
 
-CREATE TABLE `supervisors` (
-  `id` integer,
-  `admin_id` integer,
-  `first_name` varchar(255),
-  `last_name` varchar(255),
-  `email` varchar(255),
-  `password` varchar(255),
-  `birthday_date` date,
-  `company` varchar(255),
-  `employee_number` integer,
-  `is_female` boolean,
-  `profile_picture` varchar(255)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `admins` (
-  `id` integer,
-  `first_name` varchar(255),
-  `last_name` varchar(255),
-  `email` varchar(255),
-  `password` varchar(255),
-  `birthday_date` date,
-  `is_female` boolean,
-  `profile_picture` varchar(255),
-  `CGU` varchar(255)
-);
 
-CREATE TABLE `tests` (
-  `id` integer,
-  `user_id` integer,
-  `description` varchar(255),
-  `number` integer,
-  `date` datetime,
-  `score` integer,
-  `title` varchar(255),
-  `reaction_time` datetime,
-  `result` varchar(255)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `messages` (
-  `id` integer,
-  `user_id` integer,
-  `object` varchar(255),
-  `content` varchar(255),
-  `date` datetime
-);
+--
+-- Base de données :  `stimulus`
+--
 
-CREATE TABLE `faq_questions` (
-  `id` integer,
-  `supervisor_id` integer,
-  `title` varchar(255),
-  `answer` varchar(255),
-  `subject` varchar(255)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE 'cgu' (
-  `id` integer,
-  `text` varchar(255)
-);
+--
+-- Structure de la table `admins`
+--
 
-ALTER TABLE `users` ADD FOREIGN KEY (`supervisor_id`) REFERENCES `supervisors` (`id`);
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `birthday_date` date DEFAULT NULL,
+  `is_female` tinyint(1) DEFAULT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-ALTER TABLE `supervisors` ADD FOREIGN KEY (`admin_id`) REFERENCES `admins` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `tests` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+--
+-- Structure de la table `cgu`
+--
 
-ALTER TABLE `faq_questions` ADD FOREIGN KEY (`supervisor_id`) REFERENCES `supervisors` (`id`);
+DROP TABLE IF EXISTS `cgu`;
+CREATE TABLE IF NOT EXISTS `cgu` (
+  `id` int(11) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-ALTER TABLE `messages` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `faq_questions`
+--
+
+DROP TABLE IF EXISTS `faq_questions`;
+CREATE TABLE IF NOT EXISTS `faq_questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `supervisor_id` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `answer` varchar(255) DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `supervisor_id` (`supervisor_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `faq_questions`
+--
+
+INSERT INTO `faq_questions` (`id`, `supervisor_id`, `title`, `answer`, `subject`) VALUES
+(18, NULL, 'ezzaez', 'eazeaza', 'eazea');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `object` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `supervisors`
+--
+
+DROP TABLE IF EXISTS `supervisors`;
+CREATE TABLE IF NOT EXISTS `supervisors` (
+  `id` int(11) NOT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `birthday_date` date DEFAULT NULL,
+  `company` varchar(255) DEFAULT NULL,
+  `employee_number` int(11) DEFAULT NULL,
+  `is_female` tinyint(1) DEFAULT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `admin_id` (`admin_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tests`
+--
+
+DROP TABLE IF EXISTS `tests`;
+CREATE TABLE IF NOT EXISTS `tests` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `reaction_time` datetime DEFAULT NULL,
+  `result` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `supervisor_id` int(11) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `birthday_date` date DEFAULT NULL,
+  `company` varchar(255) DEFAULT NULL,
+  `employee_number` int(11) DEFAULT NULL,
+  `is_female` tinyint(1) DEFAULT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
+  `number_of_tests_done` int(11) DEFAULT NULL,
+  `needs_tests` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `supervisor_id` (`supervisor_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
