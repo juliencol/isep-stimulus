496f0e1b065f89f6d4f833f4ceda5a49d06a53ff
cyprian <cyprinanou@gmail.com>
2020-05-28 13:43:47 +0200

    petite modif cgus

--- app/views/cgus/index.php
+++ app/views/cgus/index.php
@@ -1,4 +1,4 @@
-<?php include '../../config/config.php';?>
+<?php require_once APPROOT . '/../app/config/config.php';?>
 <?php require APPROOT . '/../public/css/style.php'; ?>
 <style> <?php include APPROOT . '/../public/css/style.css'; ?> </style>
 
