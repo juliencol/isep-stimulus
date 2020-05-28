a0994dec8efdbf80256d2a3b4bdac233b24e08ea
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2020-05-28 15:53:07 +0200

    Resolution of a problem on require config.php

--- app/views/admins/sign_in.php
+++ app/views/admins/sign_in.php
@@ -1,4 +1,3 @@
-<?php require_once APPROOT . '../../config/config.php';?>
 <?php require APPROOT . '/../public/css/style.php'; ?>
 <style> <?php include APPROOT . '/../public/css/style.css'; ?> </style>
 <style> <?php include APPROOT . '/../public/css/admins/sign_in.css'; ?> </style>
