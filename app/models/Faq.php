74bc9d4fc1e441b69b6721ad863f3d4122bfe4fe
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2020-05-31 03:29:02 +0200

    Changes

--- app/models/Faq.php
+++ app/models/Faq.php
@@ -1,48 +1,90 @@
-<?php  
-  class Faq {
+<?php
+class Faq {
     private $db;
 
     public function __construct() {
-      $this->db = new Database;
+        $this->db = new Database;
     }
 
     public function Question() {
-      $this->db->query('SELECT * FROM faq_questions ');
-      return $faq_questions=$this->db->resultSet();
-      
+        $this->db->query('SELECT * FROM faq_questions ');
+        return $faq_questions=$this->db->resultSet();
+
     }
     public function addQuestion($data){
-      $this->db->query('INSERT INTO faq_questions (id,supervisor_id,title,answer,subject) VALUES (:id,:supervisor_id,:title,:answer,:subject)');
-      $this->db->bind('id', $data['id']);
-      $this->db->bind('supervisor_id', $data['supervisor_id']);
-      $this->db->bind('title', $data['title']);
-      $this->db->bind('answer', $data['answer']);
-      $this->db->bind('subject', $data['subject']);
+        $this->db->query('INSERT INTO faq_questions (id,supervisor_id,title,answer,subject) VALUES (:id,:supervisor_id,:title,:answer,:subject)');
+        $this->db->bind('id', $data['id']);
+        $this->db->bind('supervisor_id', $data['supervisor_id']);
+        $this->db->bind('title', $data['title']);
+        $this->db->bind('answer', $data['answer']);
+        $this->db->bind('subject', $data['subject']);
 
-      if ($this->db->execute()) {
-        return true;
-      } else {
-        return false;
-      }
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
     }
     public function removeQuestion($id){
-      $this->db->query('DELETE FROM faq_questions WHERE id = :id');
-      $this->db->bind(':id', $id);
-      
-      if ($this->db->execute()) {
-        return true;
-      } else {
-        return false;
-      }
+        $this->db->query('DELETE FROM faq_questions WHERE id = :id');
+        $this->db->bind(':id', $id);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
     }
     public function modifQuestion($id){
-      $this->db->query('SELECT * FROM faq_questions WHERE id= :id');
-      $this->db->bind(':id',$id);
-      var_dump($id);
-      return $question=$this->db->resultSet();
-     
-      
+        $this->db->query('SELECT * FROM faq_questions WHERE id= :id');
+        $this->db->bind(':id',$id);
+        var_dump($id);
+        return $question=$this->db->resultSet();
+
+
     }
-   
-  }
+
+    public function QuestionInvisible() {
+        $this->db->query('SELECT * FROM faq_invisible ');
+        return $faq_invisible=$this->db->resultSet();
+
+    }
+    public function addQuestionInvisible($data){
+        $this->db->query('INSERT INTO faq_invisible (id,supervisor_id,title,answer,subject) VALUES (:id,:supervisor_id,:title,:answer,:subject)');
+        $this->db->bind('id', $data['id']);
+        $this->db->bind('supervisor_id', $data['supervisor_id']);
+        $this->db->bind('title', $data['title']);
+        $this->db->bind('answer', $data['answer']);
+        $this->db->bind('subject', $data['subject']);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function removeQuestionInvisible($id){
+        $this->db->query('DELETE FROM faq_invisible WHERE id = :id');
+        $this->db->bind(':id', $id);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function uneQuestion($id) {
+        $this->db->query('SELECT * FROM faq_questions WHERE id= :id');
+        $this->db->bind(':id', $id);
+        return $faq_questions=$this->db->resultSet();
+
+    }
+    public function uneQuestionInvisible($id) {
+        $this->db->query('SELECT * FROM faq_invisible WHERE id= :id');
+        $this->db->bind(':id', $id);
+        return $faq_questions=$this->db->resultSet();
+
+    }
+
+}
 ?>
--- db/database.sql
+++ db/database.sql
@@ -1,22 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 30 mai 2020 à 23:41
--- Version du serveur :  10.4.10-MariaDB
--- Version de PHP :  7.3.12
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `stimulus`
@@ -30,7 +12,7 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `admins`;
 CREATE TABLE IF NOT EXISTS `admins` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
@@ -38,8 +20,8 @@ CREATE TABLE IF NOT EXISTS `admins` (
   `birthday_date` date DEFAULT NULL,
   `is_female` tinyint(1) DEFAULT NULL,
   `profile_picture` varchar(255) DEFAULT NULL,
-  `CGU` varchar(255) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -52,14 +34,24 @@ CREATE TABLE IF NOT EXISTS `cgu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Déchargement des données de la table `cgu`
+-- Structure de la table `faq_invisble`
 --
 
-INSERT INTO `cgu` (`id`, `content`) VALUES
-(1, 'CGU');
+DROP TABLE IF EXISTS `faq_invisble`;
+CREATE TABLE IF NOT EXISTS `faq_invisble` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `supervisor_id` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `answer` varchar(255) DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `supervisor_id` (`supervisor_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -69,20 +61,22 @@ INSERT INTO `cgu` (`id`, `content`) VALUES
 
 DROP TABLE IF EXISTS `faq_questions`;
 CREATE TABLE IF NOT EXISTS `faq_questions` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `supervisor_id` int(11) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   `answer` varchar(255) DEFAULT NULL,
   `subject` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `supervisor_id` (`supervisor_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `faq_questions`
 --
 
 INSERT INTO `faq_questions` (`id`, `supervisor_id`, `title`, `answer`, `subject`) VALUES
-(1, 2, 'Comment', 'Passer des tests', 'Question simple');
+(24, NULL, 'a', 'a', 'a'),
+(18, NULL, 'ezzaez', 'eazeaza', 'eazea');
 
 -- --------------------------------------------------------
 
@@ -92,13 +86,14 @@ INSERT INTO `faq_questions` (`id`, `supervisor_id`, `title`, `answer`, `subject`
 
 DROP TABLE IF EXISTS `messages`;
 CREATE TABLE IF NOT EXISTS `messages` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `object` varchar(255) DEFAULT NULL,
   `content` varchar(255) DEFAULT NULL,
   `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -108,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `messages` (
 
 DROP TABLE IF EXISTS `supervisors`;
 CREATE TABLE IF NOT EXISTS `supervisors` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `admin_id` int(11) DEFAULT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
@@ -119,8 +114,9 @@ CREATE TABLE IF NOT EXISTS `supervisors` (
   `employee_number` int(11) DEFAULT NULL,
   `is_female` tinyint(1) DEFAULT NULL,
   `profile_picture` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `admin_id` (`admin_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -130,8 +126,8 @@ CREATE TABLE IF NOT EXISTS `supervisors` (
 
 DROP TABLE IF EXISTS `tests`;
 CREATE TABLE IF NOT EXISTS `tests` (
-  `id` int(255) DEFAULT NULL,
-  `Type` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Type` varchar(255) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `number` int(11) DEFAULT NULL,
@@ -140,21 +136,9 @@ CREATE TABLE IF NOT EXISTS `tests` (
   `title` varchar(255) DEFAULT NULL,
   `reaction_time` datetime DEFAULT NULL,
   `result` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `tests`
---
-
-INSERT INTO `tests` (`id`, `Type`, `user_id`, `description`, `number`, `date`, `score`, `title`, `reaction_time`, `result`) VALUES
-(1, 'Son', 1, 'Test de temps de réaction à un son', 36, '2020-05-19 00:00:00', 123, 'Test en vue du pilotage d\'un Boeing', '2000-00-20 00:00:00', 'Bon résultat'),
-(9, 'time_sound', 1, 'Test simple', 36, '2020-05-26 00:00:00', 23, 'OI', '2020-05-26 00:00:00', 'BIEN'),
-(56, 'Son', 4, 'OUI', 23, '2020-05-20 00:00:00', 23, 'AAA', '2020-05-04 00:00:00', 'ASEZ'),
-(8, 'reproduct_sound', 1, 'Test général', 234, '2020-05-19 00:00:00', 23, 'AA', '2000-00-10 00:00:00', 'Bien'),
-(7, 'time_light', 1, 'AA', 123, '2020-05-27 00:00:00', 123, 'AA', '2020-05-26 00:00:00', 'ASZER'),
-(6, 'repr_sound', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(5, 'repr_sound', 1, 'Test général', 34, '2020-05-27 00:00:00', 234, 'BER', '2020-05-20 00:00:00', 'Bien');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -164,7 +148,7 @@ INSERT INTO `tests` (`id`, `Type`, `user_id`, `description`, `number`, `date`, `
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `supervisor_id` int(11) DEFAULT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
@@ -177,15 +161,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   `profile_picture` varchar(255) DEFAULT NULL,
   `number_of_tests_done` int(11) DEFAULT NULL,
   `needs_tests` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `supervisor_id` (`supervisor_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`id`, `supervisor_id`, `first_name`, `last_name`, `email`, `password`, `birthday_date`, `company`, `employee_number`, `is_female`, `profile_picture`, `number_of_tests_done`, `needs_tests`) VALUES
-(1, 2, 'Gabriel', 'ENRIQUEZ', 'enriquezgabriel426@gmail.com', NULL, '2020-05-06', 'Stimulus', 324, 0, NULL, 0, 1);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
