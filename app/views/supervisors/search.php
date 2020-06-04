d7a1902ddf45fa00cf945996427a5c7e35b91d29
cyprian <cyprinanou@gmail.com>
2020-06-05 00:22:22 +0200

    modif

--- app/views/supervisors/search.php
+++ app/views/supervisors/search.php
@@ -9,7 +9,7 @@
 
     <?php
 
-        $db = new PDO('mysql:host=localhost;dbname=isep-stimulus;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $db = new PDO('mysql:host=localhost;dbname=stimulus;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
         if (isset($_POST['research']) AND !empty($_POST['research'])) {
             $research = htmlspecialchars($_POST['research']);
