c13a76494607371326c441203c66bc7a000f0b20
Julien Colombain <julien@colombain.com>
2020-06-04 22:00:02 +0200

    test results update

--- app/views/users/test_results.php
+++ app/views/users/test_results.php
@@ -1,11 +1,11 @@
 <?php require APPROOT . '/../public/css/style.php'; ?>
 <style> <?php include APPROOT . '/../public/css/style.css'; ?> </style>
 <style> <?php include APPROOT . '/../public/css/users/test_results.css'; ?> </style>
+
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
-<html>
-    <title>Results</title>
-    <div class="container">
+<div class="container text-center">
+    <div class="">
         <br><br>
         <br><br>
        <?php
@@ -13,9 +13,9 @@
                echo $data['error_id'];
            } else { ?>
                <h1>Temps de réaction à un son</h1>
-
+    
         <?php
-
+    
         if(!empty($data['time_sound'])) {
             foreach ($data['time_sound'] as $info) { ?>
                 <aside>Vous avez effectué un test de type <?php echo $info->title."<br> le ".$info->date." et vous avez obtenu un score de ".$info->score; ?></aside>
@@ -24,14 +24,12 @@
             if(isset($data['absence_time_sound'])) {
                 echo $data['absence_time_sound'];
             }
-
+    
         } ?>
-
-
 
     <h1>Capacité à reproduire un son</h1>
         <section>
-
+    
                     <?php if (!empty($data['reproduct_sound'])) { ?>
                         <spin>
                             <?php foreach ($data['reproduct_sound'] as $info) { ?>
@@ -39,11 +37,11 @@
                                     Vous avez effectué un test <?php echo $info->title."<br> le ".$info->date." et vous avez obtenu un score de ".$info->score; ?>
                                 </aside>
                             <?php  }?>
-
-
+    
+    
                         </spin>
                         <article>
-
+    
                         <table>
                     <caption>Capacité à reproduire un son :</caption>
                     <tr>
@@ -54,19 +52,19 @@
                         <td><?php echo $info->number;?></td>
                         <td><?php echo $info->score;?></td>
                     <?php } ?>
-
-
+    
+    
                 </table>
             </article>
                     <?php } else {
                         if(isset($data['absence_reproduct_sound'])) { ?>
                             <?php echo $data['absence_reproduct_sound']; ?>
                         <?php }
-
+    
                     } ?>
-
+    
                 <br><br>
-
+    
         </section>
     <h1>Temps de réaction à une lumière</h1>
         <section>
@@ -81,19 +79,15 @@
                 if(isset($data['absence_time_light'])) {
                     echo $data['absence_time_light'];
                 }
-
+    
             }?>
-
+    
         </section>
-    <footer>
-
-    </footer>
          <?php  }
            ?>
-
-
+    </div>
 </div>
 
-</html>
+
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
